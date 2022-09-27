61d6e38469bea0440293187198cfee3dc3af216d
gurujowa <gurujowa@gmail.com>
2022-09-27 08:08:16 +0000

    changed to erc721

--- contract/contracts/CatsDappsNFT.sol
+++ contract/contracts/CatsDappsNFT.sol
@@ -1,35 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol';
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import '@openzeppelin/contracts/access/Ownable.sol';
 import "hardhat/console.sol";
 import "./IAstarBase.sol";
 
-contract CatsDappsNFT is ERC1155URIStorage, Ownable {
+contract CatsDappsNFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
+
+    string baseURI = "https://arweave.net/dcGGZIY6krXymT0m2ZG_iq2gtZslrSbQZV0EX1N7l0Y";
     uint256 public publicMaxPerTx = 1;
     mapping(address => uint256) public claimed;
     IAstarBase public ASTARBASE = IAstarBase(0x8E2fa5A4D4e4f0581B69aF2f8F2Ef2CF205aE8F0);
     address MY_STAKING_PROJECT = 0x8b5d62f396Ca3C6cF19803234685e693733f9779;
-    string public name = 'CatsDappsNFT';
-    string public symbol = 'CATD';
 
-
-    constructor() ERC1155("") {
-      setBaseURI("https://arweave.net/yqjVjrr5jhFdIeae0dA3yQRwCSAcwd1uVdhbif7Wjkk/");
-      setURI(1, "dapps.json");
+    constructor() ERC721("CatsDappsNFT", "CATD") {
     }
 
-    function setURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
-        _setURI(tokenId, _tokenURI);
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
     }
 
-    function setBaseURI(string memory baseURI) public onlyOwner {
-        _setBaseURI(baseURI);
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return baseURI;
     }
-
 
     /// @notice this function verifies staker status on a contract by using caller's account
     /// @param user caller's account
@@ -46,6 +42,7 @@ contract CatsDappsNFT is ERC1155URIStorage, Ownable {
     // public mint
     function publicMint() public {
         require(isPassHolder(msg.sender), "Claim allowed only for stakers registered in AstarPass");
+        uint256 supply = totalSupply();
 
         require(
             claimed[msg.sender] + 1 <= publicMaxPerTx,
@@ -53,18 +50,16 @@ contract CatsDappsNFT is ERC1155URIStorage, Ownable {
         );
 
         claimed[msg.sender] += 1;
-        _mint(msg.sender, 1, 1, "");
+        _safeMint(msg.sender, supply + 1);
 
     }
 
+    function ownerMint(uint256 count) public onlyOwner {
+        uint256 supply = totalSupply();
 
-    function mint(
-        address to,
-        uint256 id,
-        uint256 amount
-    ) public onlyOwner  {
-        _mint(to, id, amount, '');
+        for (uint256 i = 1; i <= count; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
     }
-
 
 }
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -4,7 +4,7 @@ import { getEnvVariable } from "./helpers";
 async function main() {
   const AstarCats = await hre.ethers.getContractFactory("CatsDappsNFT");
   console.log('Deploying AstarCats ERC721 token...');
-  const token = await AstarCats.deploy();
+  const token = await AstarCats.deploy({ gasPrice: 100000000000 });
 
   await token.deployed();
   console.log("AstarCats deployed to:", token.address);
